48296fe158fb31d6eb103049dee50e582d7819b3
atelito <aaxelporetti@gmail.com>
2020-12-23 21:13:06 -0300

    grupos

--- src/testNG/OtraPrueba.java
+++ src/testNG/OtraPrueba.java
@@ -1,62 +1,101 @@
 package testNG;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.edge.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
+/*
+ * 1. Navegar a la pagina
+ * 2. Hacer click en boton Show Alert box
+ * 3. Mostrar mensaje de alerta
+ * 4. Apretar aceptar y salir de alerta
+ * 
+ * 5. Hacer click en boton Show confirm box
+ * 6. Mostrar mensaje de alerta
+ * 7. Apretar cancelar y salir de la alerta
+ * 8. Verificar si al dar cancelar da false el texto de la pagina
+ * 
+ * 9. Hacer click en el boton Show prompt box
+ * 10. Escribir tu nombre
+ * 11. Apretar aceptar y cerrar alerta
+ * 12. Verificar si el texto en la pagina es el mismo que el introducido en la alerta
+ */
 
 
 @Test
 public class OtraPrueba {
-	public String baseUrl = "http://demo.guru99.com/test/newtours/";
-    String driverPath = "C:\\msedgedriver.exe";
-    public WebDriver driver ;
-    
-    @Test(priority = 0)
-    public void verificarTituloLandingPage() {
-        
-        String tituloEsperado = "Welcome: Mercury Tours";
-        String titulo = driver.getTitle();
-        Assert.assertEquals(titulo, tituloEsperado);
-    }
-    
-    @Test(priority = 1)
-    public void register(){
-  	  String expected, actual = null;
-        driver.findElement(By.linkText("REGISTER")).click() ;
-        expected = "Register: Mercury Tours";
-        actual = driver.getTitle();
-        Assert.assertEquals(actual, expected);
-    }
-        @Test(priority = 2)
-    public void support() {
-      	 String expected, actual = null;
-          driver.findElement(By.linkText("SUPPORT")).click() ;
-          expected = "Under Construction: Mercury Tours";
-          actual = driver.getTitle();
-          Assert.assertEquals(actual, expected);
-    }
-        
-  @BeforeMethod
-  public void f ( ) {
-}
+	public WebDriver driver;
 
-  @AfterMethod
-  public void volverLandingPage ( ) {
-      driver.findElement(By.linkText("Home")).click();
-}
+	@Test(priority = 0, groups = "alertas")
+	public void verificarBoton1() {
+		WebElement boton1 = driver.findElement(By.cssSelector("input[id='alertexamples']"));
+		boton1.click();
+		Alert alerta1 = driver.switchTo().alert();
+		System.out.println(alerta1.getText());
+		alerta1.accept();
+	}
 
-  @BeforeTest
-  public void inicio() {
-	  System.setProperty("webdriver.edge.driver", driverPath);
-      driver= new EdgeDriver();
-      driver.get(baseUrl);
-  }
+	@Test(priority = 1, groups = "alertas")
+	public void verificarBoton2() {
+		WebElement boton2 = driver.findElement(By.cssSelector("input[id='confirmexample']"));
+		boton2.click();
+		WebDriverWait espera = new WebDriverWait(driver, 10); // Explicita bajo determinadas condiciones
+		espera.until(ExpectedConditions.alertIsPresent());
+		try {
+			Alert alerta2 = driver.switchTo().alert();
+			System.out.println(alerta2.getText());
+			alerta2.dismiss();
+			Assert.assertEquals(driver.findElement(By.cssSelector("p[id='confirmreturn']")).getText(), "false");
+		} catch (TimeoutException toe) {
+			System.out.println(toe.toString());
+		}
+	}
 
+	@Test(priority = 2, groups = "alertas")
+	public void verificarBoton3() {
+		WebElement boton3 = driver.findElement(By.cssSelector("input[id='promptexample']"));
+		boton3.click();
+		WebDriverWait espera = new WebDriverWait(driver, 10); // Explicita bajo determinadas condiciones
+		espera.until(ExpectedConditions.alertIsPresent());
+		try {
+			Alert alerta3 = driver.switchTo().alert();
+			String nombre = "Atel";
+			alerta3.sendKeys(nombre);
+			alerta3.accept();
+			Assert.assertEquals(driver.findElement(By.cssSelector("p[id='promptreturn']")).getText(), nombre);
+		} catch (TimeoutException toe) {
+			System.out.println(toe.toString());
+		}
+	}
 
-  @AfterTest
-  public void finalizar() {
-	  driver.quit();
-  
-  }
+	@BeforeMethod
+	public void f() {
+		System.out.println("----------Inicio Prueba----------");
+
+	}
+
+	@AfterMethod
+	public void volverLandingPage() {
+		System.out.println("----------Fin Prueba----------");
+	}
+
+	@BeforeTest
+	public void inicio() {
+		Util datos = new Util();
+		datos.setUrlBase("http://compendiumdev.co.uk/selenium/testpages/alerts.html");
+		driver = Util.setUpManual();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.get(datos.getUrlBase());
+	}
+
+	@AfterTest
+	public void finalizar() {
+		driver.quit();
+
+	}
 
 }
---
+++ src/testNG/ParametrizacionByITestContext.java
@@ -0,0 +1,138 @@
+package testNG;
+
+import java.util.concurrent.TimeUnit;
+
+import java.util.concurrent.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.*;
+import org.openqa.selenium.support.ui.*;
+import org.testng.Assert;
+import org.testng.Assert.*;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+public class ParametrizacionByITestContext {
+	WebDriver driver = Util.setUpManual();;	
+	
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Metodo que se ejecuta antes de una suite de pruebas");
+	}
+	
+	@BeforeTest(groups = {"A", "B"})
+	public void beforeTest() {
+		System.out.println("Metodo que se ejecuta antes del conjunto de pruebas @Test");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com.ar/");
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Metodo que se ejecuta antes de la clase");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Metodo que se ejecuta antes de cada @Test");
+	}
+	
+	@Test(dataProvider = "Buscador", groups = "A")
+	public void busquedaA(String usuario, String busqueda) throws InterruptedException {
+		WebElement cajaDeBusqueda = driver.findElement(By.cssSelector("input[name='q']"));
+		cajaDeBusqueda.sendKeys(busqueda);
+		System.out.println("Usuario: " + usuario);
+		Thread.sleep(1000);
+		
+		String input = cajaDeBusqueda.getAttribute("value"); //siempre son value para sacar el texto de cuadros de busqueda
+		System.out.println("El texto buscado es: " + input + ", y la busqueda por parametro era: " + busqueda);
+		Assert.assertTrue(input.equals(busqueda), "No coinciden");
+		cajaDeBusqueda.clear();
+	}
+	
+	@Test(dataProvider = "Buscador", groups = "B")
+	public void busquedaB(String usuario, String busqueda) throws InterruptedException {
+		WebElement cajaDeBusqueda = driver.findElement(By.cssSelector("input[name='q']"));
+		cajaDeBusqueda.sendKeys(busqueda);
+		Thread.sleep(1000);
+		
+		String input = cajaDeBusqueda.getAttribute("value"); //siempre son value para sacar el texto de cuadros de busqueda
+		System.out.println("El texto buscado es: " + input + ", y la busqueda por parametro era: " + busqueda);
+		Assert.assertTrue(input.equals(busqueda), "No coinciden");
+		cajaDeBusqueda.clear();
+	}
+	
+	
+	@Test(dataProvider = "BuscadorDesdeClase", enabled = false, dataProviderClass = BuscadorDesdeClase.class)
+	public void busquedaDesdeClase(String usuario, String busqueda) throws InterruptedException {
+		WebElement cajaDeBusqueda = driver.findElement(By.cssSelector("input[name='q']"));
+		cajaDeBusqueda.sendKeys(busqueda);
+		System.out.println("Usuario: " + usuario);
+		Thread.sleep(1000);
+		
+		String input = cajaDeBusqueda.getAttribute("value"); //siempre son value para sacar el texto de cuadros de busqueda
+		System.out.println("El texto buscado es: " + input + ", y la busqueda por parametro era: " + busqueda);
+		Assert.assertTrue(input.equals(busqueda), "No coinciden");
+		cajaDeBusqueda.clear();
+	}
+	
+
+	@Test(dataProvider = "BuscadorDesdeClase", enabled = false, dataProviderClass = BuscadorDesdeClase.class)
+	public void busquedaDesdeClaseConMetodo(String busqueda) throws InterruptedException {
+		WebElement cajaDeBusqueda = driver.findElement(By.cssSelector("input[name='q']"));
+		cajaDeBusqueda.sendKeys(busqueda);
+		Thread.sleep(1000);
+		
+		String input = cajaDeBusqueda.getAttribute("value"); //siempre son value para sacar el texto de cuadros de busqueda
+		System.out.println("El texto buscado es: " + input + ", y la busqueda por parametro era: " + busqueda);
+		Assert.assertTrue(input.equals(busqueda), "No coinciden");
+		cajaDeBusqueda.clear();
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("Metodo que se ejecuta despues de cada @Test");
+	}
+	
+	@AfterClass
+	public void afterClass() {
+		System.out.println("Metodo que se ejecuta despues de la clase");
+	}
+	
+	@AfterTest(groups = {"A"})
+	public void afterTest() {
+		System.out.println("Metodo que se ejecuta despues del conjunto de pruebas @Test");
+		driver.quit();
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("Metodo que se ejecuta despues de una suite de pruebas");
+	}
+	
+	@DataProvider(name = "Buscador")
+	public Object[][] getDataProvider(ITestContext contexto){
+		Object [][] datos = null;
+		for (String grupo : contexto.getIncludedGroups()) {
+			if (grupo.equals("A")) {
+				datos = new Object [][] {
+					{"Atel", "Google"},
+					{"Vale", "Facebook"},
+					{"Andres", "Instagram"},
+					{"Emma", "WhatsApp"}
+				};
+				break;
+			}
+			else if (grupo.equals("B")) {
+				datos = new Object [][] {
+					{"Argentina"},
+					{"Francia"},
+					{"Brasil"},
+					{"EEUU"}
+				};
+				break;
+			}
+		}
+		return datos;
+	}
+	
+}
